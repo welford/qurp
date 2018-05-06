b1dea8fab422c38fb54d0b9103edf4438e110cd0
James W. Anderson <james.w.anderson@gmail.com>
2018-05-06 22:06:17 +0100

    big cleanup of makefile/buildfile, enable cross compile
    More to do, but this is a good start.
    
    ./build.sh release_gl -cc will cross compile setting the systemroot and gcc prefix to the following default values:
    
    CCPREFIX=/home/pi/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-
    CCSYSROOT=/home/pi/raspberrypi/sysroot
    
    Edit them in ./build.sh to match your machine.

--- WinQuake/build.sh
+++ WinQuake/build.sh
@@ -1,24 +1,56 @@
 #!/bin/bash
-# this script builds q1 for raspberry pi
+
+# This script builds q1 for raspberry pi
 # invoke with ./build.sh
 # or ./build.sh clean to clean before build
 
+### Globals settings ###
+
+# cross compile suff, edit to suit your local build
+
+CCPREFIX=/home/pi/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-
+CCSYSROOT=/home/pi/raspberrypi/sysroot
+
+# include directories
+
+# directory to find khronos linux make files (with include/ containing headers! Make needs them.
+# the = in I=/ is so we can make cross compiling easier as it lets up replace the start with a different
+# system root https://gcc.gnu.org/onlinedocs/gcc/Directory-Options.html
+INCLUDES="-I=/usr/include -I=/opt/vc/include -I=/opt/vc/include/interface/vcos/pthreads -I=/opt/vc/include/interface/vmcs_host/linux "
+
 # directory containing the ARM shared libraries (rootfs, lib/ of SD card)
 # specifically libEGL.so and libGLESv2.so
-ARM_LIBS=/opt/vc/lib
+ARM_LIBS="-L=/opt/vc/lib"
 
-# directory to find khronos linux make files (with include/ containing
-# headers! Make needs them.)
-INCLUDES="-I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux "
+# $# <- the number of parameters
+# $@ <- all arguments
 
-# prefix of arm cross compiler installed
-#CROSS_COMPILE=bcm2708-
+args=("$@") # <- sets args to the current arguments
+while test $# -gt 0
+do
+	case "$1" in
+		-cc)
+			echo "Cross Compile"
+			CROSS_COMPILE=$CCPREFIX
+			SYSTEM_ROOT="--sysroot="$CCSYSROOT
+			;;
+		clean)
+			#echo "Clean Build"
+			#rm -rf ./releasearm/*
+			#rm -rf ./debugarm/*
+			;;
 
-# clean
-if [ $# -ge 1 ] && [ $1 = clean ]; then
-   echo "clean build"
-   rm -rf build/*
-fi
+		-*)
+			echo "bad option $1"
+			;;
+		#*)
+		#	echo "argument $1"
+		#	;;
+	esac
+	shift
+done
+# since we shifted the args away we need to reset them
+set -- $args
 
 echo "- - - - - - - - - - - - - - - "
 echo "Start Make"
@@ -26,8 +58,8 @@ echo "- - - - - - - - - - - - - - - "
 
 make -j4 -f make.rpi $1 ARCH=arm \
 	CC=""$CROSS_COMPILE"gcc" USE_SVN=0 USE_CURL=0 USE_OPENAL=0 \
-	CFLAGS="-march=armv6 -mfpu=vfp -mfloat-abi=hard $INCLUDES" \
-	LDFLAGS="-pthread -lm -L"$ARM_LIBS" -lvchostif -lvcfiled_check -lbcm_host -lkhrn_static -lvchiq_arm -lopenmaxil -L/opt/vc/lib/ -lbrcmEGL -lbrcmGLESv2 -lvcos -lrt -lSDL -ludev"
+	CFLAGS="$SYSTEM_ROOT $INCLUDES" \
+	LDFLAGS="-lm -pthread $ARM_LIBS -lvchostif -lvcfiled_check -lbcm_host -lkhrn_static -lvchiq_arm -lopenmaxil -lbrcmEGL -lbrcmGLESv2 -lvcos -lrt -lSDL -ludev"
 	
 exit 0
 
